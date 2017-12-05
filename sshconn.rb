06f0b6e5f9c1d9c8114218117ead4174dbf9d585
Rajkumar Surabhi <mailtorajuit@gmail.com>
2017-12-05 16:39:08 +0530

    fhdsfj'

--- sshconn.rb
+++ sshconn.rb
@@ -12,12 +12,12 @@ require 'net/scp'
 begin
   puts ARGV[0]
   puts ARGV[1]
-  file = "wget -O /home/rajkumar/Downloads/"+ARGV[0]+" "+ARGV[1]
+  file = "wget -O "+ARGV[0]+" "+ARGV[1]
   puts file
   system(file)
   Net::SCP.start(@hostname, "rajsurabhi") do |scp|
     # upload a file to a remote server
-    scp.upload! "/home/rajkumar/Downloads/"+ARGV[0], "/home/rajsurabhi"
+    scp.upload! ARGV[0], "/home/rajsurabhi"
   end
   ssh = Net::SSH.start(@hostname, @username, :host_key=>"ssh-rsa",:keys => [ "rajutest.pub"], :forward_agent=>true)
   res = ssh.exec!(@cmd)
