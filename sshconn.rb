fd739c82de8ccb6fbbf662dea25caf139bceb812
Rajkumar Surabhi <mailtorajuit@gmail.com>
2017-12-13 12:37:01 +0530

    script fixed

--- sshconn.rb
+++ sshconn.rb
@@ -5,8 +5,8 @@ require 'net/scp'
 #gem install net-ssh
 #gem install net-scp
 
-@hostname = "35.188.131.223"
-@username = "rajsurabhi"
+@hostname = "35.201.139.242"
+@username = "mailtorajuit"
 @cmd = "ls -al"
 @password = "oddTulip@123"
 begin
@@ -15,13 +15,13 @@ begin
   file = "wget -O #{ARGV[0]} #{ARGV[1]}"
   puts file
   system(file)
-  Net::SCP.start(@hostname, "rajsurabhi") do |scp|
+  Net::SCP.start(@hostname, "mailtorajuit") do |scp|
     # upload a file to a remote server
-    scp.upload! ARGV[0], "/home/rajsurabhi"
+    scp.upload! ARGV[0], "/home/mailtorajuit"
   end
-  ssh = Net::SSH.start(@hostname, @username, :host_key=>"ssh-rsa",:keys => [ "rajutest.pub"], :forward_agent=>true)
+  ssh = Net::SSH.start(@hostname, "mailtorajuit", :host_key=>"ssh-rsa",:keys => [ "/home/rajkumar/.ssh/id_rsa.pub"], :forward_agent=>true)
   res = ssh.exec!(@cmd)
-  ssh.exec!("sudo cp /home/rajsurabhi/"+ARGV[0]+" /var/lib/tomcat8/webapps/")
+  ssh.exec!("sudo cp /home/mailtorajuit/"+ARGV[0]+" /var/lib/tomcat8/webapps/")
   ssh.exec!("sudo service tomcat8 restart")
   ssh.close
   puts res
