d76c37a2ac69245028908f97dc053ebc0da888ba
Rajkumar Surabhi <mailtorajuit@gmail.com>
2017-12-28 11:14:59 +0530

    script

--- sshconn.rb
+++ sshconn.rb
@@ -5,7 +5,7 @@ require 'net/scp'
 #gem install net-ssh
 #gem install net-scp
 
-@hostname = "35.194.172.75"
+@hostname = "35.194.233.140"
 @username = "mailtorajuit"
 @cmd = "ls -al"
 @password = "oddTulip@123"
@@ -15,12 +15,13 @@ begin
   file = "wget -O #{ARGV[0]} #{ARGV[1]}"
   puts file
   system(file)
+  puts "FIle downloaded using wget"
   Net::SCP.start(@hostname, "mailtorajuit", :password => @password) do |scp|
     # upload a file to a remote server
     scp.upload! ARGV[0], "/home/mailtorajuit"
   end
   puts "File uploaded using scp"
-  ssh = Net::SSH.start(@hostname, "mailtorajuit", :host_key=>"ssh-rsa",:keys => ["id_rsa.pub"], :forward_agent=>true)
+  ssh = Net::SSH.start(@hostname, "mailtorajuit", :host_key=>"ssh-rsa",:keys => ["rajutest.pub"], :forward_agent=>true)
   res = ssh.exec!(@cmd)
   ssh.exec!("sudo service tomcat8 stop")
   ssh.exec!("sudo cp /home/mailtorajuit/"+ARGV[0]+" /var/lib/tomcat8/webapps/")
